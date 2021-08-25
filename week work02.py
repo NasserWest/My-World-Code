3a1c4b7337e457a6ae3573de89c9347bee103f6a
NasserWest <88714809+NasserWest@users.noreply.github.com>
2021-08-25 17:58:24 +0700

    week_02
    python edit

---
+++ week work02.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+c=int
+c = (2*a)+b
+print("%d\n" %c)
+
+a=int(input())
+c = (a**5)+(a**4)+(2*(a**3))+(3*(a**2))+(4*a)+3
+print("%d\n" %c)
+
+a=int(input())
+c = 2+(3*a)
+print("%d" %c)

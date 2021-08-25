74c3b9f147cb03c7392fc5c805f33e3c2f64da2a
NasserWest <88714809+NasserWest@users.noreply.github.com>
2021-08-25 17:57:32 +0700

    week_02
    week_02

---
+++ week work_02.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int a,b,c;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	
+	 c = (2*a)+b;
+		printf("%d\n",c);
+	
+	 
+	scanf("%d",&a);
+	 c = pow(a,5)+pow(a,4)+(2*pow(a,3))+(3*pow(a,2))+(4*a)+3;
+		printf("%d\n",c);
+	
+	scanf("%d",&a);
+	 c = 2+(3*a);
+	 	printf("%d",c);
+	
+	
+	
+	
+	
+	return 0;
+}

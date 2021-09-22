60b384fbb70482d451903e440c3f75e4d77c78df
NasserWest <88714809+NasserWest@users.noreply.github.com>
2021-09-22 22:33:06 +0700

    Week05
    C++

---
+++ week05_V.2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int x,y,z;
+		printf("Enter number:");
+		scanf("%d",&y);
+				for(x=0;x < y;x++){
+					printf("\n");
+				for(z=0;z < y;z++)
+					printf("*");
+			}
+	return 0;		
+}
